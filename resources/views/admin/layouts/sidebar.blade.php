2954c19fac02ac1040e964ae57ab87273e387f21
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-12-11 17:24:01 +0400

    Added icons

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,13 +1,22 @@
 <nav id="sidebar">
     <ul class="list-unstyled components">
         <li class="nav-item active">
-            <a class="nav-link nav_link" href="{{route('admin.home')}}">Dashboard</a>
+            <a class="nav-link nav_link" href="{{route('admin.home')}}">
+                <i class="me-1 fa-solid fa-chart-line"></i>
+                Dashboard
+            </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link nav_link" href="{{route('categories.index')}}">Categories</a>
+            <a class="nav-link nav_link" href="{{route('categories.index')}}">
+                <i class="me-1 fa-solid fa-list"></i>
+                Categories
+            </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link nav_link" href="{{route('products.index')}}">Products</a>
+            <a class="nav-link nav_link" href="{{route('products.index')}}">
+                <i class="me-1 fa-solid fa-shirt"></i>
+                Products
+            </a>
         </li>
     </ul>
 </nav>
