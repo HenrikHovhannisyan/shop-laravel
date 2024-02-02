631f5df16183db7d9b60b2b4ddaaf5d1751eb52f
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-02-02 17:44:20 +0400

    Added home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,42 @@
 @endsection
 
 @section('content')
-    <h1>Shop Page</h1>
-{{--    @include('layouts.footer')--}}
+    <div class="container">
+        <section id="banner">
+            <button>MEN'S NEW ARRIVALS</button>
+        </section>
+        <section id="gift">
+            <h2>THE GIFT GUIDE</h2>
+            <p>UNWRAP LUXURY.</p>
+            <div class="gift_container">
+                <a href="#">
+                    <img src="../img/gift.webp" alt="">
+                    <p class="text-dark"><u>gifts for him</u></p>
+                </a>
+            </div>
+        </section>
+        <section id="shop">
+            <h2>COS ATELIER 2023</h2>
+            <a href="#">
+                <p class="text-dark mt-3">SHOP MEN</p>
+            </a>
+            <a href="">
+                <img src="../img/man.webp" alt="">
+            </a>
+        </section>
+        <section id="more">
+            <h2>More to explore</h2>
+            <div class="d-flex justify-content-center align-items-center">
+                <a href="#" class="border border-dark ps-3 pe-3 m-1">
+                    <p class="text-dark">Men's Outerwear</p>
+                </a>
+                <a href="#" class="border border-dark ps-3 pe-3 m-1">
+                    <p class="text-dark">Men's Knitwear</p>
+                </a>
+            </div>
+        </section>
+    </div>
+    @include('layouts.footer')
 @endsection
 
 @section('script')
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,60 +1,16 @@
-<section id="footer">
+<section id="footer" class="bg-white">
     <div class="container">
         <footer class="row py-5">
-            <div class="col-12 col-md-3 mb-3">
-                <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-                    <img src="{{ asset('./img/footer_logo.png') }}" class="img-fluid" style="max-height: 45px" alt="Logo">
-                </a>
-                <a href="{{$info[0]['facebook']}}" class="footer_soc">
-                    <i class="fa-brands fa-facebook"></i>
-                </a>
-                <a href="{{$info[0]['instagram']}}" class="footer_soc">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </div>
-            <div class="col-12 col-md-2 mb-3">
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2">
-                        <a href="#" class="footer_link p-0">Products</a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <a href="#" class="footer_link p-0">Projects</a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <a href="#" class="footer_link p-0">Showroom</a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <a href="#" class="footer_link p-0">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-12 col-md-5 mb-3">
-                <ul class="nav flex-column">
-                    <li class="nav-item mb-2">
-                        <a href="mailto:{{$info[0]['email']}}" class="footer_link p-0">
-                            <i class="fa-regular fa-envelope"></i>
-                            {{$info[0]['email']}}
-                        </a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <a href="tel:{{$info[0]['phone1']}}" class="footer_link p-0">
-                            <i class="fa-solid fa-phone"></i>
-                            {{$info[0]['phone1']}}
-                        </a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <a href="tel:{{$info[0]['phone2']}}" class="footer_link p-0">
-                            <i class="fa-solid fa-phone"></i>
-                            {{$info[0]['phone2']}}
-                        </a>
-                    </li>
-                    <li class="nav-item mb-2">
-                        <p class="footer_link p-0">
-                            <i class="fa-solid fa-location-dot"></i>
-                            {{$info[0]['address']}}
-                        </p>
-                    </li>
-                </ul>
+            <hr class="mt-5 mb-5">
+            <div class="d-flex justify-content-between align-items-center">
+                <a href="" class="text-dark">Contact Us</a>
+                <div class="d-flex">
+                    <span class="pe-2">
+                        <p class="text-dark"><b>Enjoy 10% off your</b></p>
+                        <p class="text-dark"><b>first order</b></p>
+                    </span>
+                    <button class="bg-dark text-white ps-5 pe-5">Subscribe</button>
+                </div>
             </div>
         </footer>
     </div>
